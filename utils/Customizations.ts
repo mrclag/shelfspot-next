db50a5a8008acdd0f66a9b67dee5d33bd11d32bc
Matt Clagett <mattrclagett@gmail.com>
2022-12-13 17:00:14 -0500

    update imports

--- utils/Customizations.ts
+++ utils/Customizations.ts
@@ -1,10 +1,10 @@
-import bamboo from "../../public/static/img/shelfdecorations/bamboo.png";
-import budda from "../../public/static/img/shelfdecorations/budda.png";
-import cacti from "../../public/static/img/shelfdecorations/cacti.png";
-import leafy from "../../public/static/img/shelfdecorations/leafy.png";
-import plant from "../../public/static/img/shelfdecorations/plant.png";
-import pusheen from "../../public/static/img/shelfdecorations/pusheen.png";
-import spikey from "../../public/static/img/shelfdecorations/spikey.png";
+import bamboo from "../public/static/img/shelfdecorations/bamboo.png";
+import budda from "../public/static/img/shelfdecorations/budda.png";
+import cacti from "../public/static/img/shelfdecorations/cacti.png";
+import leafy from "../public/static/img/shelfdecorations/leafy.png";
+import plant from "../public/static/img/shelfdecorations/plant.png";
+import pusheen from "../public/static/img/shelfdecorations/pusheen.png";
+import spikey from "../public/static/img/shelfdecorations/spikey.png";
 
 export const shelfDecorations = [
   { id: 0, icon: plant },
