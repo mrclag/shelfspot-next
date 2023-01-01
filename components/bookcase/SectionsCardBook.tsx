75cb15668a9a4d5c9990ae646191c794feb42519
Matt Clagett <mattrclagett@gmail.com>
2022-12-31 22:45:38 -0800

    Add height to books on dashboard bookcase

--- components/bookcase/SectionsCardBook.tsx
+++ components/bookcase/SectionsCardBook.tsx
@@ -3,18 +3,25 @@ import React from "react";
 const SectionsCardBook = ({ book }) => {
   let width;
   if (book && book.pageCount) {
-    const num = Math.min(Math.max(book.pageCount / 7, 25), 60);
+    const num = Math.min(Math.max(book.pageCount / 7, 25), 45);
     width = num + "px";
   } else {
     width = "20px";
   }
 
-  // let height = Math.random() * 40 + 70;
+  const bookImg = new Image();
+  let bookImage = book.imageLinks[0]?.smallThumbnail;
+  bookImg.src = bookImage;
+  const bookImgHeight = bookImg.height;
+  const bookImgWidth = bookImg.width;
+  const ratio = bookImgHeight / bookImgWidth;
+  const distFromOne = bookImgHeight > bookImgWidth ? ratio - 1 : 1 - ratio;
+  const imageHeight = Math.max(Math.min(70 + distFromOne * 60, 120), 60) + "px";
 
   return (
     <div
       className="section-card-book"
-      style={{ width: width, background: book.color[0]  }}
+      style={{ width: width, background: book.color[0], height: imageHeight }}
     ></div>
   );
 };
